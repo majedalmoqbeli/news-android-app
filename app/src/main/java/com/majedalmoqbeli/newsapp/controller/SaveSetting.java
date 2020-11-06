5f0b1bb5cba2ee839ae8fa81c748c8d4fb5e9ee5
majedalmoqbeli <majedalmoqbeli@gmail.com>
2020-11-06 19:33:40 +0300

    Image news display optimization.

--- app/src/main/java/com/majedalmoqbeli/newsapp/controller/SaveSetting.java
+++ app/src/main/java/com/majedalmoqbeli/newsapp/controller/SaveSetting.java
@@ -8,9 +8,6 @@ public class SaveSetting {
     // this is for localhost
     public static String ServerURL = "http://192.168.137.1:80/news/";
 
-    //public static String ServerURL = "https://news.majedalmoqbeli/api/v1/";
-
-
     public static String ImageUserURL = ServerURL + "/user_images/";
     public static String ImageNewsURL = ServerURL + "/news_images/";
 
